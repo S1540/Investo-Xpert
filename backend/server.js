e50111b63e29d9e6c0cf24d038414cef79e6da77
Shubham Singh <shubhamkumar2452004@gmail.com>
2025-12-12 11:01:01 +0530

    diploy

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,16 @@ const propertyRoutes = require("./routes/propertyRoutes");
 dotenv.config();
 
 app.use(cors());
-app.use(express.json()); // Only for normal JSON
-app.use("/uploads", express.static("uploads")); // Image serve
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 app.use("/api", propertyRoutes);
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
